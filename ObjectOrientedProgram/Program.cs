dc62a56da2a9fa464462641aa67bef011cd5b0f6
karanmaurya15 <karanmaurya9663@gmail.com>
2022-10-31 11:00:51 +0530

    Wellcome Program

---
+++ ObjectOrientedProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ObjectOrientedProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Object Oriented Program");
+        }
+    }
+}
